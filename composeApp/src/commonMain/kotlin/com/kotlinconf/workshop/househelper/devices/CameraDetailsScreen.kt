e8ef81504c5768c63e261322c8d2885053c15bb8
Márton Braun <marton.braun@jetbrains.com>
2025-07-22 11:24:43 +0200

    Implement image loading with Coil

--- composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/devices/CameraDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/devices/CameraDetailsScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -36,8 +38,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import com.kotlinconf.workshop.househelper.VideoPlayer
 import com.kotlinconf.workshop.househelper.rememberVideoPlayerState
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
@@ -122,6 +132,21 @@ fun CameraDetailsScreen(
                         videoPlayerState.stop()
                     }
                 }
+
+                val footageUrl by viewModel.cameraFootage.collectAsStateWithLifecycle()
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalPlatformContext.current)
+                        .data(footageUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "Camera image",
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                        .clip(MaterialTheme.shapes.medium)
+                        .aspectRatio(16f / 9f)
+                        .fillMaxWidth(),
+                    contentScale = ContentScale.Crop,
+                    colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply { setToSaturation(0f) }),
+                )
 
 //                VideoPlayer(
 //                    url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
